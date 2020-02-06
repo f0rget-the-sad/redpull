a2e75f820e4b4f550fd961371cfb4b2beb6bba68
Volodymyr Fialko <volodymyrf@interfacemasters.com>
2020-02-06 19:03:16 +0200

    connect to IMT-redmine

---
+++ getall.py
@@ -0,0 +1,5 @@
+from redminelib import Redmine
+
+passwd = getpass.getpass()
+
+redmine = Redmine('https://redmine.interfacemasters.com/redmine', username='volodymyrf', password='osuthebest2015', requests={'verify': False})
